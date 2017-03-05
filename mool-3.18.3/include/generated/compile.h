f24a89bc10f7b4416b4591ebe4eabf8571768a3c
SMLsweety <sweeshimal@gmail.com>
2017-03-05 16:01:46 +0530

    points to mool3.18

---
+++ mool-3.18.3/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 65 */
+/* SMP */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#65 SMP Fri Jan 23 19:21:44 IST 2015"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "boss"
+#define LINUX_COMPILER "gcc version 4.4.5 (GCC) "
